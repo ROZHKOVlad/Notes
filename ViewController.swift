c7c0be348d66cf8003ab7cbef5b27e79acb4dc70
ROZHKOVlad <90374574+ROZHKOVlad@users.noreply.github.com>
2024-03-17 18:53:05 +0300

    Initial commit

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Notes
+//
+//  Created by Влад Рожко on 17.03.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
